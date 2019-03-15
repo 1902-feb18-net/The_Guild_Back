166d8d58f519f0748b3e3a8545ae3815c7fe7992
Matthew Valencia <MatthewLuisValencia@gmail.com>
2019-03-15 15:36:50 -0500

    Added project skeleton

---
+++ The_Guild_Back/The_Guild_Back.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace The_Guild_Back.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ The_Guild_Back/The_Guild_Back.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace The_Guild_Back.DAL
+{
+    public class Class1
+    {
+    }
+}
