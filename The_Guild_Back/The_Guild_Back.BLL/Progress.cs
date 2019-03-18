0f8ab5cd3052b92bb408ba798a73e04853c5cac2
Lee <ljmars2351@gmail.com>
2019-03-18 16:18:39 -0500

    adding new saves

--- The_Guild_Back/The_Guild_Back.BLL/Progress.cs
+++ The_Guild_Back/The_Guild_Back.BLL/Progress.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using Ardalis.GuardClauses;
 
 namespace The_Guild_Back.BLL
 {
     public class Progress
     {
-
+        private string _name;
         public int Id { get; set; }
-        public string Nam { get; set; }
+        public string Nam
+        {
+            get => _name;
+            set
+            {
+                Guard.Against.NullOrWhiteSpace(value, nameof(value));
+                _name = value;
+            }
+        }
     }
 }
--- The_Guild_Back/The_Guild_Back.BLL/Users.cs
+++ The_Guild_Back/The_Guild_Back.BLL/Users.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using Ardalis.GuardClauses;
 
 namespace The_Guild_Back.BLL
 {
     public class Users
     {
-        
+        private string _firstName, _lastName;
 
         public int Id { get; set; }
         public int LoginInfoId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                Guard.Against.NullOrWhiteSpace(value, nameof(value));
+                _firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                Guard.Against.NullOrWhiteSpace(value, nameof(value));
+                _lastName = value;
+            }
+        }
         public decimal? Salary { get; set; }
         public int? Strength { get; set; }
         public int? Dex { get; set; }
