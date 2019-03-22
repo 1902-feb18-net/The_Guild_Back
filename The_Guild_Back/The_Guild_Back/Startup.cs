2c819e976fd611ed312affac16f1905d62cab2d4
Lee <ljmars2351@gmail.com>
2019-03-22 16:56:55 -0500

    adding swagger

--- The_Guild_Back/The_Guild_Back/Startup.cs
+++ The_Guild_Back/The_Guild_Back/Startup.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 using The_Guild_Back.DAL;
 
 namespace The_Guild_Back.API
@@ -29,7 +33,37 @@ namespace The_Guild_Back.API
         {
             services.AddDbContext<AuthDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("AuthDb")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            { }).AddEntityFrameworkStores<AuthDbContext>();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "UserServiceAuth";
+                options.ExpireTimeSpan = TimeSpan.FromHours(2);
+                options.Events = new CookieAuthenticationEvents
+                {
+                    OnRedirectToLogin = context =>
+                    {
+                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                        context.Response.Headers.Remove("Location");
+                        return Task.FromResult(0);
+                    }
+                };
+            });
+
+            services.AddAuthentication();
+
+            services.AddMvc(options =>
+            {
+                options.ReturnHttpNotAcceptable = true;
+            }).AddXmlSerializerFormatters()
+              .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Character API", Version = "v1" });
+            });
 
         }
 
@@ -45,6 +79,15 @@ namespace The_Guild_Back.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseAuthentication();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Character API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
