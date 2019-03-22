695a7a7cd884547772793e5ad3ee7ef78ec0b026
Lee <ljmars2351@gmail.com>
2019-03-22 09:24:23 -0500

    more identify additions

--- The_Guild_Back/The_Guild_Back/Controllers/AccountController.cs
+++ The_Guild_Back/The_Guild_Back/Controllers/AccountController.cs
@@ -2,16 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using The_Guild_Back.API.Models;
+using The_Guild_Back.DAL;
 
 namespace The_Guild_Back.API.Controllers
 {
-    public class AccountController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
     {
-        public IActionResult Index()
+        public SignInManager<IdentityUser> SignInManager { get; }
+
+        public AccountController(SignInManager<IdentityUser> signInManager,
+            AuthDbContext dbContext)
         {
-            return View();
+            // we can do code-first "skipping" migrations at runtime
+            // the downside is, we can't run migrations on the database that gets generated
+            // later.
+            dbContext.Database.EnsureCreated();
+            SignInManager = signInManager;
+        }
+
+        // POST for create resource, but also for "perform operation"
+        // when there is no way to fit the operation into CRUD terms.
+        // POST /account/login
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login(Login login)
+        {
+            var result = await SignInManager.PasswordSignInAsync(
+                login.Username, login.Password, login.RememberMe, false);
+
+            if (!result.Succeeded)
+            {
+                return Unauthorized(); // 401 for login failure
+            }
+
+            return NoContent();
+        }
+
+        // POST /account/logout
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Logout()
+        {
+            await SignInManager.SignOutAsync();
+
+            return NoContent();
+        }
+
+        // POST /account
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterUser register,
+            [FromServices] UserManager<IdentityUser> userManager)
+        {
+            var user = new IdentityUser(register.Username);
+
+            var result = await userManager.CreateAsync(user, register.Password);
+
+            if (!result.Succeeded) // e.g. did not meet password policy
+            {
+                return BadRequest(result);
+            }
+
+            await SignInManager.SignInAsync(user, false);
+
+            return NoContent(); // nothing to show the user that he can access
         }
     }
 }
---
+++ The_Guild_Back/The_Guild_Back/Models/RegisterUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace The_Guild_Back.API.Models
+{
+    public class RegisterUser
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal? Salary { get; set; }
+        public int? Strength { get; set; }
+        public int? Dex { get; set; }
+        public int? Wisdom { get; set; }
+        public int? Intelligence { get; set; }
+        public int? Charisma { get; set; }
+        public int? Constitution { get; set; }
+        public int? RankId { get; set; }
+    }
+}
