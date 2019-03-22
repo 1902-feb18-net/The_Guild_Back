2838be92bf247739b28f85f798a338e38a64d786
Lee <ljmars2351@gmail.com>
2019-03-22 14:00:31 -0500

    hopefully fix launch

--- The_Guild_Back/The_Guild_Back/Models/RegisterUser.cs
+++ The_Guild_Back/The_Guild_Back/Models/RegisterUser.cs
@@ -12,13 +12,5 @@ namespace The_Guild_Back.API.Models
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public decimal? Salary { get; set; }
-        public int? Strength { get; set; }
-        public int? Dex { get; set; }
-        public int? Wisdom { get; set; }
-        public int? Intelligence { get; set; }
-        public int? Charisma { get; set; }
-        public int? Constitution { get; set; }
-        public int? RankId { get; set; }
     }
 }
