92672fd30f29f2038a4e8b39d6f04b9133c6bbc6
esqueda6021 <o_e10@outlook.com>
2019-03-22 13:45:31 -0500

    AddRepo

---
+++ The_Guild_Back/The_Guild_Back.BLL/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Guild_Back.BLL
+{
+    public interface IRepository
+    {
+        void AddUser(DAL.Users users);
+
+        void AddLoginInfo(DAL.LoginInfo loginInfo);
+
+        IEnumerable<Users> GetAllUsers();
+        IEnumerable<LoginInfo> GetAllLoginInfo();
+
+
+
+    }
+
+   
+
+
+}
---
+++ The_Guild_Back/The_Guild_Back.BLL/UserRepo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using The_Guild_Back.DAL;
+
+namespace The_Guild_Back.BLL
+{
+    public class UserRepo : IRepository
+    {
+        private readonly project2theGuildContext _db;
+
+        public UserRepo(project2theGuildContext db)
+        {
+            _db = db;
+        }
+
+        public void AddUser(DAL.Users users)
+        {
+            _db.Add(Mapper.Map(users));
+        }
+
+        public IEnumerable<Users> GetAllUsers()
+        {
+            return Mapper.Map(_db.Users);
+        }
+
+        
+        public void DeleteUser(int Id)
+        {
+            _db.Remove(_db.Users.Find(Id));
+        }
+
+
+        public void AddLoginInfo(DAL.LoginInfo loginInfo)
+        {
+            _db.Add(Mapper.Map(loginInfo));
+        }
+
+        public IEnumerable<LoginInfo> GetAllLoginInfo()
+        {
+            return Mapper.Map(_db.LoginInfo);
+        }
+
+    }
+}
