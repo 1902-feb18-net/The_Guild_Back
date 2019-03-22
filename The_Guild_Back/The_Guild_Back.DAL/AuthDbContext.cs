e81fca5c9a58c8840d87e621c00f529e1079750b
Lee <ljmars2351@gmail.com>
2019-03-21 19:43:44 -0500

    starting work on auth

---
+++ The_Guild_Back/The_Guild_Back.DAL/AuthDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace The_Guild_Back.DAL
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        // the parent class already has all the right dbsets etc
+
+        public AuthDbContext(DbContextOptions options) : base(options)
+        { }
+    }
+}
