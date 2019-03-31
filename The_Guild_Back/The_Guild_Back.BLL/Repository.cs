ccd7b0e65885e7f22130827af37f1c3a01181dda
Matthew Valencia <MatthewLuisValencia@gmail.com>
2019-03-31 18:45:23 -0500

    Added more checks on updating user. Added adventurerpart and requesting group controllers with gets for angular/gm.

--- The_Guild_Back/The_Guild_Back.BLL/Repository.cs
+++ The_Guild_Back/The_Guild_Back.BLL/Repository.cs
@@ -64,7 +64,12 @@ namespace The_Guild_Back.BLL
             var mapped = Mapper.Map(user);
             var current = await _db.Users.FindAsync(user.Id);
 
-            if(mapped.UserName != current.UserName)
+            if (current == null)
+            {
+                throw new ArgumentException("User does not exist.");
+            }
+
+            if (mapped.UserName != current.UserName)
             {
                 throw new ArgumentException("Cannot change username");
             }
@@ -72,6 +77,11 @@ namespace The_Guild_Back.BLL
             if(mapped.RankId != current.RankId) //if they're trying to rank up
             {
                 var requirements = await _db.RankRequirements.FindAsync(current.RankId);
+
+                if(requirements == null)
+                {
+                    throw new ArgumentException("New rank does not exist.");
+                }
 
                 //minimum total stats check
                 int userStats = 0;
